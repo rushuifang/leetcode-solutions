c15207274522bed088fd55b1646b1b44c90dacea
Rushui Fang <rfang4@binghamton.edu>
2020-10-26 17:22:06 -0400

    10.26.20

---
+++ 18. 4Sum/Solution.py
@@ -0,0 +1,71 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def kSum(nums, target, k):
+            res = []
+            if len(nums) == 0 or nums[0] * k > target or nums[-1] * k < target:
+                return res
+            if k == 2:
+                return twoSum(nums, target)
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for _, set in enumerate(
+                        kSum(nums[i + 1 :], target - nums[i], k - 1)
+                    ):
+                        res.append([nums[i]] + set)
+            return res
+
+        def twoSum(nums, target):
+            res = []
+            low, high = 0, len(nums) - 1
+            while low < high:
+                s = nums[low] + nums[high]
+                if (s < target) or (low > 0 and nums[low] == nums[low - 1]):
+                    low += 1
+                elif (s > target) or (
+                    high < len(nums) - 1 and nums[high] == nums[high + 1]
+                ):
+                    high -= 1
+                elif s == target:
+                    res.append([nums[low], nums[high]])
+                    low += 1
+                    high += 1
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
+
+
+# Solution provided
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
+            res = []
+            if len(nums) == 0 or nums[0] * k > target or target > nums[-1] * k:
+                return res
+            if k == 2:
+                return twoSum(nums, target)
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for _, set in enumerate(
+                        kSum(nums[i + 1 :], target - nums[i], k - 1)
+                    ):
+                        res.append([nums[i]] + set)
+            return res
+
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            res = []
+            lo, hi = 0, len(nums) - 1
+            while lo < hi:
+                sum = nums[lo] + nums[hi]
+                if sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                    lo += 1
+                elif sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+                    hi -= 1
+                else:
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
