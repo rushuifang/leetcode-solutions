ec7dc66da6b97ef23859d178b44c9d928b76744f
Rushui Fang <rfang4@binghamton.edu>
2021-01-02 16:04:07 -0500

    01.02.21

--- 234. Palindrome Linked List/Solution.py
+++ 234. Palindrome Linked List/Solution.py
@@ -32,3 +32,22 @@ class Solution(object):
             slow = slow.next
             fast = fast.next
         return True
+
+
+# Another solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        self.left = head
+        return self.traverse(head)
+        
+    def traverse(self, right):
+        if right == None:
+            return True
+        res = self.traverse(right.next) and right.val == self.left.val
+        self.left = self.left.next
+        return res
--- 392. Is Subsquence/Solution.py
+++ 392. Is Subsquence/Solution.py
@@ -6,3 +6,31 @@ class Solution:
             else:
                 return False
         return True
+
+# Follow up
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        def binary(target, arr):
+            low = 0
+            high = len(arr)
+            while low < high:
+                mid = (low + high) // 2
+                if arr[mid] >= target:
+                    high = mid
+                else:
+                    low = mid + 1
+            return low
+        
+        index = collections.defaultdict(list)
+        for idx, char in enumerate(t) :
+            index[char].append(idx)
+            
+        j = 0
+        for char in s:
+            if char not in index:
+                return False
+            pos = binary(j, index[char])
+            if pos == len(index[char]):
+                return False
+            j = index[char][pos] + 1
+        return True
