ae5681b9f44ac16d2558821025a56c2722d05a48
Rushui Fang <rfang4@binghamton.edu>
2021-01-11 10:58:23 -0500

    01.11.21

---
+++ 626. Exchange Seats/Solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    (CASE 
+        WHEN mod(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN mod(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (SELECT COUNT(*) AS counts FROM seat) AS seat_counts
+ORDER BY id ASC;
